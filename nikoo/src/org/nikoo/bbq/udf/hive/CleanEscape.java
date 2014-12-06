0ee5e5a58432da7ca16838efaa787ea1b92b5a26
surajit-paul <paul.surajit@gmail.com>
2014-12-06 09:05:07 -0800

    Signed-off-by: surajit-paul <paul.surajit@gmail.com>

---
+++ nikoo/src/org/nikoo/bbq/udf/hive/CleanEscape.java
@@ -0,0 +1,27 @@
+package org.nikoo.bbq.udf.hive;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.Text;
+/**
+ * Hive UDF to eliminate back slash from text
+ * @author Surajit Paul
+ *@version 1.0
+ */
+public class CleanEscape extends UDF {
+	
+	public Text evaluate(Text input) {
+	    if(input == null) return null;
+	    String escape = "\\";
+	    String field = input.toString();
+	    
+	    while(escape.equalsIgnoreCase(String.valueOf(field.charAt(field.length() - 1)))){
+	    	field = field.substring(0, field.length() - 1);
+	    	if(field.equalsIgnoreCase("")) {
+	    		field = "NULL";
+	    		return new Text(field);
+	    	}
+		}			    
+	    return new Text(field);
+	 }
+}
+
